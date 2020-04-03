8b8545dec9487ba264558664ec11c3b3cc29b079
Paulo Alves <paulo.r.b.a.alves@hotmail.com>
2020-04-03 12:02:27 -0300

    testes unitarios

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,18 +1,29 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     it('It should be able to create a new ONG', async () => {
         const response = await request(app)
-        .post('/ongs')
-        .send({
+            .post('/ongs')
+            //.set('Authorization', 'NOME DA ONG')
+            .send({
                 name: "Sei la",
                 email: "contato@contato.com",
                 whatsapp: "4700000000",
                 city: "Recife",
                 uf: "PE"
-        });
+            });
 
-        console.log(response.body);
+        expect(response.body).toHaveProperty('id');
     });
 });
